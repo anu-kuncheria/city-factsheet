3e84bf0a3c20a14a6dccc04ca37eaf29eb718df0
Anu Kuncheria <akuncheria@ses7616872.local>
2023-12-18 15:00:02 -0800

    report version 2

--- city-factsheet-sanfranciscobayarea/results/run.sh
+++ city-factsheet-sanfranciscobayarea/results/run.sh
@@ -1,6 +1,6 @@
 set -e  
 
-for cityname in napa palo_alto cupertino dublin fremont santa_clara; do
+for cityname in cupertino dublin fremont santa_clara santa_rosa south_san_francisco walnut_creek livermore los_gatos pleasanton richmond vallejo sunnyvale; do
     echo "======== Running ${cityname} ========"
 
     python3 ../scripts/etl.py "$(pwd)/city_config/$cityname" #the city config file is the argument to python script
@@ -10,7 +10,13 @@ for cityname in napa palo_alto cupertino dublin fremont santa_clara; do
     python3 ../scripts/truck.py "$(pwd)/city_config/$cityname" 
     python3 ../scripts/links_reclassification.py "$(pwd)/city_config/$cityname"
     python3 ../scripts/triplegs_metrics.py "$(pwd)/city_config/$cityname"
-    python3 ../scripts/report.py "$(pwd)/city_config/$cityname"
+
+    if python3 ../scripts/report.py "$(pwd)/city_config/$cityname"; then
+        echo "report.py executed successfully."
+    else
+        echo "report.py failed. Running Report version2 for non Coc cities"
+        python3 ../scripts/report_nococ_cities.py "$(pwd)/city_config/$cityname"
+    fi    
     echo ".. Completed .."
 done
 
